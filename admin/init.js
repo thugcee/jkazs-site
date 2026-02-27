0871ae933cadd95e8a90f41379f7fa32c8c90ae0
thugcee <20202+thugcee@users.noreply.github.com>
2026-02-27 12:37:58 +0000

    Deploying to gh-pages from @ thugcee/jkazs-site@13f73e26a7236289671b361963f99a12cc051712 🚀

---
+++ admin/init.js
@@ -0,0 +1,8 @@
+// Import widgets (optional)
+const { CMS, initCMS: init } = window
+
+// Register widgets
+// CMS.registerWidget('markdown', CMS.MarkdownWidget);
+
+// Initialize CMS with custom config
+CMS.init();
