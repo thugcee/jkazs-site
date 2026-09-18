aabb594521ce453a838eb465a060e36188435654
thugcee <20202+thugcee@users.noreply.github.com>
2026-09-18 21:33:10 +0000

    Deploying to gh-pages from @ thugcee/jkazs-site@7cb2d00ec5e8efa8c0ddef78df46cc63b9910a82 🚀

---
+++ page/informacje-o-przystani/cropping.sh
@@ -0,0 +1,3 @@
+convert jkazs-map-osm.png -crop 3839x4323+2173+2530 jkazs-map-osm-copped.png
+convert jkazs-map-osm-copped.png  -resize 50% jkazs-map-osm-copped-small.png
+# square has to be done in gimp
